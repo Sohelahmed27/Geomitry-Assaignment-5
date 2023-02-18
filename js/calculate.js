2b55c38977590683a060ded9aeea1fb8adb46c52
Sohelahmed27 <sohel.gebcu@gmail.com>
2023-02-18 18:32:41 +0600

    third

--- js/calculate.js
+++ js/calculate.js
@@ -3,29 +3,37 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
   console.log(base);
   const height = getElementById('height-tri');
   const result =(.5*base*height).toFixed(2);
-  const container = document.getElementById('result-card');
-  const li = document.createElement('li');
-  li.innerHTML = `
-  <li>Triangle: ${result} cm <sup>2</sup></li>
-  `
-  container.appendChild(li);
-  
-  // const newResult = document.createElement('div');
-
-
+  addToCard('Triangle Area', result);
 })
 document.getElementById('btn-rectagle').addEventListener('click', function(){
-  console.log('triangle clicked');
+  const width = getElementById('width-rec');
+  const length = getElementById('length-rec');
+  const result =(width*length).toFixed(2);
+  addToCard('Rectangle Area', result);
 })
 document.getElementById('btn-parallogram').addEventListener('click', function(){
-  console.log('triangle clicked');
+  const base = getElementById('base-para');
+  const height = getElementById('height-para');
+  const result =(base*height).toFixed(2);
+  addToCard('Parallogram', result);
 })
 document.getElementById('btn-rhombus').addEventListener('click', function(){
-  console.log('triangle clicked');
+  const digonal1 = getElementById('digonal1');
+  const digonal2 = getElementById('digonal2');
+  const result =(.5*digonal1*digonal2).toFixed(2);
+  addToCard('rhombus Area', result);
 })
 document.getElementById('btn-pentagon').addEventListener('click', function(){
-  console.log('btn-pentagon');
+  const base = getElementById('base-penta');
+  const height = getElementById('peri-penta');
+  const result =(.5*base*height).toFixed(2);
+  addToCard('Pentagon Area', result);
 })
 document.getElementById('btn-ecllipse').addEventListener('click', function(){
-  console.log('btn-ecllipse');
+  const base = getElementById('base-tri');
+  console.log(base);
+  const height = getElementById('height-tri');
+  const result =(.5*base*height).toFixed(2);
+  addToCard('Triangle', result);
+
 })
--- js/common.js
+++ js/common.js
@@ -4,3 +4,16 @@ function getElementById(elementId) {
   const element = parseFloat(elementString);
   return element;
 }
+
+function addToCard(AreaName, result){
+  const container = document.getElementById('result-card');
+  const li = document.createElement('li');
+  li.innerHTML = `
+  <ol>
+  <li>${AreaName}: ${result} cm <sup>2</sup></li>
+  </ol>
+  `
+  container.appendChild(li);
+  
+  // const newResult = document.createElement('div');
+}
